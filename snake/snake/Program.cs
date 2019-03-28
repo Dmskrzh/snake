4fe96515edea9062cbd1bee0caae577b859b3490
Victor Skrzh <vvskr@tyt.by>
2019-03-28 21:51:50 +0300

    Lesson-11

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -12,9 +12,9 @@ namespace snake
 
             //Отрисовка рамки
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 20, '+');
-            VerticalLine leftLine = new VerticalLine(0, 20, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 20, 78, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             upLine.Drow();
             downLine.Drow();
             leftLine.Drow();
@@ -25,17 +25,17 @@ namespace snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
         }
 
     }       
